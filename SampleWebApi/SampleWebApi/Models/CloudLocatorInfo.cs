e3a94a6a0625061015572a260caa87ab7fe60b4c
mathew.levac <mathew.levac@NBMATHEWL>
2016-04-20 16:49:25 -0400

    Added some comments.

--- SampleWebApi/SampleWebApi/Models/CloudLocatorInfo.cs
+++ SampleWebApi/SampleWebApi/Models/CloudLocatorInfo.cs
@@ -18,13 +18,16 @@ namespace SampleWebApi.Models
         /// <summary>
         /// The 3 letter ISO country code of the address on the account.
         /// </summary>
-        public string ThreeLetterCountryCode { get; set; }
+        public string CountryCode { get; set; }
 
         /// <summary>
         /// The 2 letter province (for Canada) or state (for United States) code.
         /// </summary>
-        public string TwoLetterRegion { get; set; }
+        public string RegionCode { get; set; }
 
+        /// <summary>
+        /// If set, overrides the cloud locator logic and creates the VSC in the specified cloud.
+        /// </summary>
         public string CloudOverride { get; set; }
     }
 }
